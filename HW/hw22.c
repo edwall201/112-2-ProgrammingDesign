d4deca913d162716e3dbaabf1d9317d1ba465bb8
EDWARD-TUF\Edward <edwall201@gmail.com>
2024-05-05 17:02:52 +0800

    finished hw22

--- HW/hw22.c
+++ HW/hw22.c
@@ -12,48 +12,39 @@ int check_next_k(char *s, int start, int k, char case_to_check) {
 }
 
 int find_max_length(char *s, int k) {
-    int max_length = 0;
-    int count = 0;
-    char last_case = '\0';
-    int length = strlen(s);
-    int score_table[51] = {0};
+    int max_length = 0, i = 0, n = strlen(s);
+    while (i <= n - k) {
+        int start = i;
+        int current = 0;
+        int last_case = isupper(s[start]);  // Determine the initial expected case
 
-    for (int i = 0; i < length; i++) {
-        if ((isupper(s[i]) && last_case == 'U') || (islower(s[i]) && last_case == 'L')) {
-            count++;
-        } else {
-            count = 1;
-            
-        }
-
-        if (count == k) {
-            char next_case = isupper(s[i]) ? 'L' : 'U';
-            if (i + k < length && check_next_k(s, i + 1, k, next_case)) {
-                count = 0;
-                max_length += k;
-                // printf("%c %d\n", s[i], i);
+        while (start + k <= n) {
+            if (check_next_k(s, start, k, last_case ? 'U' : 'L')) {
+                current += k;
+                start += k;
+                last_case = !last_case;  // Toggle the case expectation
             } else {
-                count = 1;
-                
+                break;  // Segment did not match, break to try a new starting point
             }
         }
 
-        last_case = isupper(s[i]) ? 'U' : 'L';
+        if (current > max_length) max_length = current;
+        if (current == 0) i++;  // No valid segment found at the current start, move by 1
+        else i = start - k + 1;  // Start again from the last valid k segment's next position
+        
     }
-
+    if(max_length == k) max_length =0;
     return max_length;
 }
 
 int main() {
     int N;
     scanf("%d", &N);
-
     for (int i = 0; i < N; i++) {
         char s[51];
         int k;
         scanf("%s %d", s, &k);
         printf("%d\n", find_max_length(s, k));
     }
-
     return 0;
 }
