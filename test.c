aa384cb326abcd134ca6e19f3b7120ce99a11871
EDWARD-TUF\Edward <edwall201@gmail.com>
2024-06-19 00:22:58 +0800

    pract q2

--- test.c
+++ test.c
@@ -5,53 +5,52 @@ typedef struct node_s {
     int time;
     // struct node_s * nexts[30];
     int nextCount;
-    int next[30];
+    int nexts[30];
 } task_t;
 typedef task_t * pTask;
 
 void dfs(int u, task_t node[], int dp[], int pre[]){
     for (int i = 0; i < node[u].nextCount; i++)
     {
-        int v = node[u].next[i];
-        if(dp[v] < node[v].time + dp[u]){
-            dp[v] = node[v].time + dp[u];
+        int v = node[u].nexts[i];
+        if(dp[v] < dp[u] + node[v].time){
+            dp[v] = dp[u] + node[v].time;
             pre[v] = u;
             dfs(v, node, dp, pre);
         }
     }
+    
 }
-
-int max(int a, int b) {return (a>b)? a:b;}
-void printPath(int pre[], int end_node){
-    if(pre[end_node] == -1) {
-        printf("%d", end_node);
+int max(int a, int b){return (a>b)? a:b;}
+void printTree(int pre[], int end){
+    if(pre[end] == -1) {
+        printf("%d", end);
         return;
     }
-    printPath(pre, pre[end_node]);
-    printf(" %d",end_node);
+    printTree(pre, pre[end]);
+    printf("%d", end);
 }
 int main(){
     int command, M;
     scanf("%d", &command);
     scanf("%d", &M);
-    task_t tasks[M+1];
-    int pre[M+1];
+    task_t nodes[M+1];
     int dp[M+1];
-    memset(pre,-1,  sizeof(pre));
+    int pre[M+1];
     memset(dp, 0, sizeof(dp));
-    for (int i = 1; i <= M; i++)
+    memset(pre, -1, sizeof(pre));
+    for (int i =1 ; i <= M; i++)
     {
-        scanf("%d", &tasks[i].time);
-        dp[i] = tasks[i].time;
-        scanf("%d", &tasks[i].nextCount);
-        for (int j = 0; j < tasks[i].nextCount; j++)
+        scanf("%d %d", &nodes[i].time, &nodes[i].nextCount);
+        dp[i] = nodes[i].time;
+        for (int j = 0; j < nodes[i].nextCount; j++)
         {
-            scanf("%d", &tasks[i].next[j]);
+            scanf("%d", &nodes[i].nexts[j]);
         }
     }
     for (int i = 1; i <= M; i++)
     {
-        dfs(i, tasks, dp, pre);
+        dfs(i, nodes, dp, pre);
     }
     if(command == 1){
         int min = 0;
@@ -60,20 +59,17 @@ int main(){
             min = max(min, dp[i]);
         }
         printf("%d\n", min);
-    }
-    else if(command == 2){
+    }else if(command == 2){
         int max = 0;
         int end_node = 1;
         for (int i = 1; i <= M; i++)
         {
-            if(dp[i] > max) {
+            if(dp[i] > max){
                 max = dp[i];
                 end_node = i;
             }
         }
-        printPath(pre, end_node);
+        printTree(pre, end_node);
+        
     }
-    
-    
-
 }
