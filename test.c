28d0d465c55b49d2470914adc2d0fc0da6e60e82
EDWARD-TUF\Edward <edwall201@gmail.com>
2024-06-19 00:51:19 +0800

    pract polynomial

--- test.c
+++ test.c
@@ -1,75 +1,115 @@
 #include <stdio.h>
+#include <math.h>
 #include <string.h>
-typedef struct node_s {
-    int id;
-    int time;
-    // struct node_s * nexts[30];
-    int nextCount;
-    int nexts[30];
-} task_t;
-typedef task_t * pTask;
+#include <stdlib.h>
+#define shapeText(TYPE) char name[10]; \
+    int (*perimeter)(struct TYPE*); \
+    int (*area)(struct TYPE*);
+typedef struct shape_s { shapeText(shape_s); } shape_t;
+typedef struct circle_s {
+    shapeText(circle_s);
+    int radius;
+} circle_t;
 
-void dfs(int u, task_t node[], int dp[], int pre[]){
-    for (int i = 0; i < node[u].nextCount; i++)
-    {
-        int v = node[u].nexts[i];
-        if(dp[v] < dp[u] + node[v].time){
-            dp[v] = dp[u] + node[v].time;
-            pre[v] = u;
-            dfs(v, node, dp, pre);
-        }
-    }
-    
+typedef struct rectangle_s{
+    shapeText(rectangle_s);
+    int width, height;
+} rectangle_t;
+
+typedef struct square_s{
+    shapeText(square_s);
+    int side;
+} square_t;
+
+typedef struct triangle_s {
+    shapeText(triangle_s);
+    int s1, s2, s3;
+} triangle_t;
+
+int circlePerimeter(circle_t *c){return 8*c->radius;}
+int circleArea(circle_t *c){return 4*c->radius*c->radius;}
+int rectanglePerimeter(rectangle_t *r){return 2*(r->height + r->width);}
+int rectangleArea(rectangle_t *r){return (r->height * r->width);}
+int squarePerimeter(square_t *s){return 4*s->side ;}
+int squareArea(square_t *s){return s->side*s->side ;}
+int trianglePerimeter(triangle_t *t){return t->s1 +t->s2 +t->s3;}
+int triangleArea(triangle_t *t){
+    int s = (t->s1 +t->s2 +t->s3)/2;
+    return sqrt(s * (s-t->s1) * (s-t->s2) * (s-t->s3));}
+
+shape_t * newShape(char *data){
+    if(strcmp(data, "circle") == 0){
+        circle_t *c = (circle_t*)malloc(sizeof(circle_t));
+        strcpy(c->name, data);
+        c->perimeter = circlePerimeter;
+        c->area = circleArea;
+        return (shape_t *)c;
+    }else if(strcmp(data, "rectangle") == 0){
+        rectangle_t *c = (rectangle_t*)malloc(sizeof(rectangle_t));
+        strcpy(c->name, data);
+        c->perimeter = rectanglePerimeter;
+        c->area = rectangleArea;
+        return (shape_t *)c;
+    }else if(strcmp(data, "square") == 0){
+        square_t *c = (square_t*)malloc(sizeof(square_t));
+        strcpy(c->name, data);
+        c->perimeter = squarePerimeter;
+        c->area = squareArea;
+        return (shape_t *)c;
+    }else if(strcmp(data, "triangle") == 0){
+        triangle_t *c = (triangle_t*)malloc(sizeof(triangle_t));
+        strcpy(c->name, data);
+        c->perimeter = trianglePerimeter;
+        c->area = triangleArea;
+        return (shape_t *)c;
+    }else return NULL;
 }
-int max(int a, int b){return (a>b)? a:b;}
-void printTree(int pre[], int end){
-    if(pre[end] == -1) {
-        printf("%d", end);
-        return;
-    }
-    printTree(pre, pre[end]);
-    printf("%d", end);
-}
-int main(){
-    int command, M;
-    scanf("%d", &command);
-    scanf("%d", &M);
-    task_t nodes[M+1];
-    int dp[M+1];
-    int pre[M+1];
-    memset(dp, 0, sizeof(dp));
-    memset(pre, -1, sizeof(pre));
-    for (int i =1 ; i <= M; i++)
+void sort(shape_t *shapes[], int count){
+    for (int i = 0; i < count; i++)
     {
-        scanf("%d %d", &nodes[i].time, &nodes[i].nextCount);
-        dp[i] = nodes[i].time;
-        for (int j = 0; j < nodes[i].nextCount; j++)
+        for (int j = 0; j < count - 1 - i; j++)
         {
-            scanf("%d", &nodes[i].nexts[j]);
-        }
-    }
-    for (int i = 1; i <= M; i++)
-    {
-        dfs(i, nodes, dp, pre);
-    }
-    if(command == 1){
-        int min = 0;
-        for (int i = 1; i <= M; i++)
-        {
-            min = max(min, dp[i]);
-        }
-        printf("%d\n", min);
-    }else if(command == 2){
-        int max = 0;
-        int end_node = 1;
-        for (int i = 1; i <= M; i++)
-        {
-            if(dp[i] > max){
-                max = dp[i];
-                end_node = i;
+            if( (shapes[j]->perimeter(shapes[j]) < shapes[j+1]->perimeter(shapes[j+1])) ||
+            ((shapes[j]->perimeter(shapes[j]) == shapes[j+1]->perimeter(shapes[j+1])) && (shapes[j]->area(shapes[j]) < shapes[j+1]->area(shapes[j+1])) )){
+                shape_t *temp = shapes[j];
+                shapes[j] = shapes[j+1];
+                shapes[j+1] = temp;
             }
         }
-        printTree(pre, end_node);
-        
     }
+    int primeter = 0, area =0;
+    for (int i = 0; i < count; i++)
+    {
+        primeter += shapes[i]->perimeter(shapes[i]);
+        area += shapes[i]->area(shapes[i]);
+        printf("%s %d %d\n",shapes[i]->name, shapes[i]->perimeter(shapes[i]), shapes[i]->area(shapes[i]));
+    }
+    printf("%d %d\n", primeter, area);
+    
+    
+}
+int main(){
+    int count = 0;
+    scanf("%d", &count);
+    shape_t *shapes[count];
+    for (int i = 0; i < count; i++)
+    {
+        char type[100];
+        scanf("%s", type);
+        shapes[i] = newShape(type);
+        if(strcmp(type, "circle") == 0){
+            circle_t *c = (circle_t *)shapes[i];
+            scanf("%d", &c->radius);
+        }else if(strcmp(type, "rectangle") == 0){
+            rectangle_t *c = (rectangle_t*)shapes[i];
+            scanf("%d %d", &c->height, &c->width);
+        }else if(strcmp(type, "square") == 0){
+            square_t *c = (square_t*)shapes[i];
+            scanf("%d", &c->side);
+        }else if(strcmp(type, "triangle") == 0){
+            triangle_t *c = (triangle_t*)shapes[i];
+            scanf("%d %d %d", &c->s1, &c->s2, &c->s3);
+        }
+    }
+    sort(shapes, count);
 }
